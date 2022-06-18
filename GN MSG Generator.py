b509e910482d8f749f06e084c21eecbc474b1d73
SurfyCrescent97 <107763166+SurfyCrescent97@users.noreply.github.com>
2022-06-18 16:46:56 +0100

    Add files via upload

---
+++ GN MSG Generator.py
@@ -0,0 +1,144 @@
+#For My Bestie :)
+#every # is a comment code still works SO YAY
+import random
+Format = random.randint(1,5)
+
+if Format == 1:
+    FirstLine = "have a great night and good night"
+elif Format == 2:
+    FirstLine = "have a good night and great night"
+if Format == 3:
+    FirstLine = "have a awesome night and great night"
+elif Format == 4:
+    FirstLine = "have a good night and awesome night"
+if Format == 5:
+    FirstLine = "have a great night and awesome night"
+
+
+from datetime import date
+import calendar
+curr_date = date.today()
+Weekday = calendar.day_name[curr_date.weekday()]
+#:D I will try make this accruate as possible to how I would type :D so i'll add different day things 
+
+if Weekday == "Monday":
+    Format2 = random.randint(1,3)
+    if Format2 == 1:
+       SecondLine = " Uhh Cya Tuesday ily❤ :)"
+    elif Format2 == 2:
+       SecondLine = " :D cya tmr :))"
+    elif Format2 == 3:
+       SecondLine = " :D once again ily❤ and cya tmr"
+
+if Weekday == "Tuesday":
+    Format2 = random.randint(1,3)
+    if Format2 == 1:
+       SecondLine = "Ayy Tuesday cya Wednesday ily❤"
+    elif Format2 == 2:
+       SecondLine = " :D cya tmr :))"
+    elif Format2 == 3:
+       SecondLine = "Eh Wednesday tmr"
+
+if Weekday == "Wednesday":
+    Format2 = random.randint(1,3)
+    if Format2 == 1:
+       SecondLine = "Ayy Wednesday cya Thursday ily❤"
+    elif Format2 == 2:
+       SecondLine = " :D cya tmr :))"
+    elif Format2 == 3:
+       SecondLine = "Ayy :D Thursday TMR"
+
+
+if Weekday == "Thursday":
+    Format2 = random.randint(1,3)
+    if Format2 == 1:
+       SecondLine = "WOOHOO ALMOST FRIDAY cya then ilyy❤"
+    elif Format2 == 2:
+       SecondLine = " :D cya tmr :))"
+    elif Format2 == 3:
+       SecondLine = "FRIDAY AYY"
+
+
+if Weekday == "Friday":
+    Format2 = random.randint(1,3)
+    if Format2 == 1:
+       SecondLine = " HEHE TMR IS THE WEEKEND WOOHOO!"
+    elif Format2 == 2:
+       SecondLine = " :D Saturday yayy!! cya then :)"
+    elif Format2 == 3:
+       SecondLine = " weekend meaning I am home :D so yay"
+       
+if Weekday == "Saturday":
+    Format2 = random.randint(1,3)
+    if Format2 == 1:
+         SecondLine = " :D Sunday tmr :/ even if schools the day after"
+    elif Format2 == 2:
+        SecondLine = " :D Cya Sunday :D"
+    elif Format2 == 3:
+         SecondLine = " Sunday woohoo :D cya tmr :)))"
+
+if Weekday == "Sunday":
+    Format2 = random.randint(1,3)
+    if Format2 == 1:
+       SecondLine = " Ughh :( School tmr also hope to cya tmr"
+    elif Format2 == 2:
+       SecondLine = " Aww :( School tmr also hope to cya tmr"
+    elif Format2 == 3:
+       SecondLine = " ehh hope you enjoy your day at school and hope to cya tmr :)"
+
+
+#Really Random Notes here :D don't mind it
+
+print("Alright Bestie ily❤ and hope you " + FirstLine + SecondLine)
+Format = random.randint(1,5)
+
+#Talk to me pls
+if Format == 1:
+    ThirdLine = ":D remember to drop hello I'll be happy to talk about your day"
+elif Format == 2:
+    FirstLine = "Uhh drop a hello i'll be happy to talk"
+if Format == 3:
+    ThirdLine = ":D uhh yeah :) talk to me usual :D i'll be more than happy"
+elif Format == 4:
+    ThirdLine = ":) I'll be happy to talk with you tmr :D for now sleep"
+if Format == 5:
+    ThirdLine = "ilysm :) talk to you tmr :D"
+
+#Birthday :D
+import calendar
+import datetime
+month = datetime.date.today().month
+day = datetime.date.today().day
+if month == 3:
+    if day == 22:
+       BirthdayLine = (" Also HAPPY BIRTHDAY WOOO :D hope u had a great one today")
+elif month == 3:
+    if day == 21:
+       BirthdayLine = (" Also your b-day tmr :D hope your excited")
+       #EVEN A TMR LINE HAHA
+else:
+ BirthdayLine = ("")
+
+#New Line lmao
+print("\n" + ThirdLine + BirthdayLine + " as well as :) hope you enjoyed this AI generated msg, I worked a long time to do the code for it" + "\n" + "Anyhow :D")
+
+Format = random.randint(1,5)
+
+if Format == 1:
+    LastLine = "ilysmm :D cya tmr"
+elif Format == 2:
+    LastLine = "ily cya tmr :D"
+if Format == 3:
+    LastLine = ":) now rest well"
+elif Format == 4:
+    LastLine = ":D you'll be always my favourite"
+if Format == 5:
+    LastLine = ":D you'll be always my favourite"
+    #4 + 5 same cuz I want it to be 2/5 of making it say ur my favourite
+print("\n" + LastLine)
+
+
+#Features :D saying the begginging lines using days to determine what to say, birthday wishing
+
+
+
